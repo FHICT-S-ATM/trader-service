a7ac3983c74abc823763e638d1b3b3007cbc7fab
Raphael van Marion <raphaeldarealmvp@gmail.com>
2023-05-12 09:53:18 +0200

    Initial API/ flask setup

---
+++ app.py
@@ -0,0 +1,19 @@
+import datetime
+import pickle
+from eurusdyfinance import *
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/healthpoint')
+def healthpoint():
+    return 'Endpoint is healthy!'
+
+@app.route('/submit', methods=['POST'])
+def return_data():
+	data = request.get_json()
+	format = '%Y-%m-%d'
+	fromDate = datetime.strptime(data['fromDate'], format)
+	toDate = datetime.strptime(data['toDate'], format)
+ 
+	return pickle.dumps(execute(fromDate, toDate))
